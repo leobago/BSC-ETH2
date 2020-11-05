d0500222a26a11c9576464f15a2f23416950aa47
Cortze <mikelcortes@cortze>
2020-11-05 17:45:12 +0100

    Updated plotting scripts supporting now relative paths; logs/csv inputs for the clientMonitor and Shell scripts to run them

--- ETH2-clients-comparison/scripts/plotting_scripts/plotMetrics.py
+++ ETH2-clients-comparison/scripts/plotting_scripts/plotMetrics.py
@@ -6,45 +6,58 @@ import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#from datetime import datetime
 from datetime import datetime
+from pathlib import Path
 import csv
 
 
 def main ():
-    lightFile = sys.argv[1]
-    tekuFile = sys.argv[2]
-    nimbusFile = sys.argv[3]
-    prysmFile = sys.argv[4]
+    csvORlogs = sys.argv[1]
+    lightFile = Path(__file__).parent / sys.argv[2]
+    tekuFile =  Path(__file__).parent / sys.argv[3]
+    nimbusFile = Path(__file__).parent / sys.argv[4]
+    prysmFile = Path(__file__).parent / sys.argv[5]
     
-    # Generate panda from lighthouse and create csv file
-    lightPanda = getMetricsFromFile('lighthouse',lightFile)
-    print("Lighthouse -> PandaObject : Done")
-    lightPanda.to_csv('../../data/client_metrics/lighthouseMetrics.csv')
-    print("Created lighthouseMetrics.csv in data/client_metrics")
+    if csvORlogs == 'logs':
+        # Generate panda from lighthouse and create csv file
+        lightPanda = getMetricsFromFile('lighthouse',lightFile)
+        print("Lighthouse -> PandaObject : Done")
+        lightP = Path(__file__).parent / '../../data/Lighthouse/lighthouseMetrics.csv' 
+        lightPanda.to_csv(lightP)
+        print("Created lighthouseMetrics.csv in data/Nimbus")
+        
+        # Generate panda from teku and create csv file
+        tekuPanda  = getMetricsFromFile('teku',tekuFile)
+        print("Teku -> PandaObject : Done")
+        tekuP = Path(__file__).parent / '../../data/Teku/tekuMetrics.csv' 
+        tekuPanda.to_csv(tekuP)
+        print("Created tekuMetrics.csv in data/Nimbus")
+        
+        # Generate panda from teku and create csv file
+        nimbusPanda = getMetricsFromFile('nimbus', nimbusFile)
+        print("Nimbus -> PandaObject : Done")
+        nimbusP = Path(__file__).parent / '../../data/Nimbus/nimbusMetrics.csv'
+        nimbusPanda.to_csv(nimbusP)
+        print("Created nimbusMetrics.csv in data/Nimbus")
+        
+        # Generate panda from teku and create csv file
+        prysmPanda = getMetricsFromFile('prysm', prysmFile)
+        print("Prysm -> PandaObject : Done")
+        prysmP = Path(__file__).parent / '../../data/Prysm/prysmMetrics.csv'
+        prysmPanda.to_csv(prysmP)
+        print("Created nimbusMetrics.csv in data/Prysm")
+        
+    elif csvORlogs == 'csv':
+        lightPanda = pd.read_csv(lightFile)
+        tekuPanda = pd.read_csv(tekuFile)
+        nimbusPanda = pd.read_csv(nimbusFile)
+        prysmPanda = pd.read_csv(prysmFile)
+        
+    else:
+        print("csv or logs must be specified on the first argument")
+        exit()
     
-    # Generate panda from teku and create csv file
-    tekuPanda  = getMetricsFromFile('teku',tekuFile)
-    print("Teku -> PandaObject : Done")
-    lightPanda.to_csv('../../data/client_metrics/tekuMetrics.csv')
-    print("Created tekuMetrics.csv in data/client_metrics")
     
-    # Generate panda from teku and create csv file
-    nimbusPanda = getMetricsFromFile('nimbus', nimbusFile)
-    print("Nimbus -> PandaObject : Done")
-    lightPanda.to_csv('../../data/client_metrics/nimbusMetrics.csv')
-    print("Created nimbusMetrics.csv in data/client_metrics")
-    
-    # Generate panda from teku and create csv file
-    prysmPanda = getMetricsFromFile('prysm', prysmFile)
-    print("Prysm -> PandaObject : Done")
-    lightPanda.to_csv('../../data/client_metrics/prysmMetrics.csv')
-    print("Created nimbusMetrics.csv in data/client_metrics")
-    
-    print(lightPanda)
-    print(tekuPanda)
-    print(nimbusPanda)
-    print(prysmPanda)
     
     # Plot
     plotMetricsFromPanda(lightPanda, tekuPanda, nimbusPanda, prysmPanda)
@@ -113,6 +126,7 @@ def getMetricsFromFile(clientType, inputFile):
     
 def plotMetricsFromPanda(lightPanda, tekuPanda, nimbusPanda, prysmPanda):
     # Plot MEM on the same graph
+    figurePath =  Path(__file__).parent / '../../figures/metrics_plots/MEM.pdf'
     ax = lightPanda.plot(figsize=(20,10), x='TIME', y='MEM', label='Lighthouse')
     tekuPanda.plot(ax=ax, x='TIME', y='MEM', label='teku')
     nimbusPanda.plot(ax=ax, x='TIME', y='MEM', label='nimbus')
@@ -121,9 +135,11 @@ def plotMetricsFromPanda(lightPanda, tekuPanda, nimbusPanda, prysmPanda):
     plt.ylabel("System Memory Used by the client (MB)")
     plt.title("System Memory Usage Comparison Between Clients")
     plt.legend()
+    plt.savefig(figurePath)
     plt.show()
     
     # Plot CPU on the same graph
+    figurePath =  Path(__file__).parent / '../../figures/metrics_plots/CPU.pdf'
     ax = lightPanda.plot(figsize=(20,10), x='TIME', y='CPU', style='.', marker='.', markersize=0.8, label='Lighthouse')
     tekuPanda.plot(ax=ax, x='TIME', y='CPU',style='.', marker='.', markersize=0.8, label='teku')
     nimbusPanda.plot(ax=ax, x='TIME', y='CPU',style='.', marker='.', markersize=0.8, label='nimbus')
@@ -132,9 +148,11 @@ def plotMetricsFromPanda(lightPanda, tekuPanda, nimbusPanda, prysmPanda):
     plt.ylabel("CPU Usage (%)")
     plt.title("CPU Usage Comparison Between Clients")
     plt.legend(markerscale=20)
+    plt.savefig(figurePath)
     plt.show()
     
     # Plot NETIN on the same graph
+    figurePath =  Path(__file__).parent / '../../figures/metrics_plots/NETIN.pdf'
     ax = lightPanda.plot(figsize=(20,10), x='TIME', y='NETIN', label='Lighthouse')
     tekuPanda.plot(ax=ax, x='TIME', y='NETIN', label='teku')
     nimbusPanda.plot(ax=ax, x='TIME', y='NETIN', label='nimbus')
@@ -143,9 +161,11 @@ def plotMetricsFromPanda(lightPanda, tekuPanda, nimbusPanda, prysmPanda):
     plt.ylabel("Network Incoming Traffic (MB)")
     plt.title("Network Incoming Traffic Comparison Between Clients")
     plt.legend()
+    plt.savefig(figurePath)
     plt.show()
     
     # Plot NETOUT on the same graph
+    figurePath =  Path(__file__).parent / '../../figures/metrics_plots/NETOUT.pdf'
     ax = lightPanda.plot(figsize=(20,10), x='TIME', y='NETOUT', label='Lighthouse')
     tekuPanda.plot(ax=ax, x='TIME', y='NETOUT', label='teku')
     nimbusPanda.plot(ax=ax, x='TIME', y='NETOUT', label='nimbus')
@@ -154,9 +174,11 @@ def plotMetricsFromPanda(lightPanda, tekuPanda, nimbusPanda, prysmPanda):
     plt.ylabel("Network Outcoming Traffic (MB)")
     plt.title("Network Outcoming Traffic Comparison Between Clients")
     plt.legend()
+    plt.savefig(figurePath)
     plt.show()
     
     # Plot DISK on the same graph
+    figurePath =  Path(__file__).parent / '../../figures/metrics_plots/DISK.pdf'
     ax = lightPanda.plot(figsize=(20,10), x='TIME', y='DISK', label='Lighthouse')
     tekuPanda.plot(ax=ax, x='TIME', y='DISK', label='teku')
     nimbusPanda.plot(ax=ax, x='TIME', y='DISK', label='nimbus')
@@ -165,6 +187,7 @@ def plotMetricsFromPanda(lightPanda, tekuPanda, nimbusPanda, prysmPanda):
     plt.ylabel("Disk Usage (MB)")
     plt.title("Disk Usage Comparison Between Clients")
     plt.legend()
+    plt.savefig(figurePath)
     plt.show()
 
 main()
